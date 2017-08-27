80c582b828bb7cf0c5dc5653abb27d5681c82ff6
PRATIKLAPTOP\Pratik <patelhub@github.com>
2017-08-28 00:14:46 +0530

    schema manager added

--- server/app.js
+++ server/app.js
@@ -8,17 +8,14 @@ var { authenticate } = require('./middleware/authenticate');
 var apiRoutes = require('./api');
 
 var app = express();
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+// res.header("Access-Control-Allow-Credentials", true);
+// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
