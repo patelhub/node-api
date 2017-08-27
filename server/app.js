68ad9bcc7b9071e0632e63523f3f63e16bfce85b
PRATIKLAPTOP\Pratik <patelhub@github.com>
2017-08-28 00:02:33 +0530

    schema manager added

--- server/app.js
+++ server/app.js
@@ -8,11 +8,16 @@ var { authenticate } = require('./middleware/authenticate');
 var apiRoutes = require('./api');
 
 var app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(apiRoutes);
 
-const server  = require('http').createServer(app);
+const server = require('http').createServer(app);
 const port = process.env.PORT;
 const ip = process.env.IP;
 setImmediate(() => {
