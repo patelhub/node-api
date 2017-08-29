f559c9b911eef6ab430326c36aeb1a8118f553fe
PRATIKLAPTOP\Pratik <patelhub@github.com>
2017-08-30 00:14:01 +0530

    allow delete req.

--- server/app.js
+++ server/app.js
@@ -13,10 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // res.header("Access-Control-Allow-Credentials", true);
-// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
     next();
 });
 app.use(apiRoutes);
